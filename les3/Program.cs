e1b979a430e9b47975ae3d055f488f36570d7ee0
Abashin Aleksey <mmilasomm@gmail.com>
2022-09-17 21:08:07 +0300

    Программа выводит всё чётные числа задача 8

---
+++ les3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Чётные числа
+Console.Clear();
+Console.WriteLine("Введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if ( a % 2 == 1)
+{
+    Console.WriteLine("Число : " + a + " не чётное");
+}
+else
+{
+     Console.WriteLine("Число : " + a + " чётное");
+}
---
+++ les5/Program.cs
@@ -0,0 +1,10 @@
+﻿// программа находит чётные числа20
+Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+Console.WriteLine("Чётные числа от 1 до " + num);
+while (count <= num)
+{
+    Console.Write($"{count}");
+    count = count + 2;
+}  
