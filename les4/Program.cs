b682fb5ac68ce8b732dcecff6705035568b0d031
Abashin Aleksey <mmilasomm@gmail.com>
2022-09-17 20:42:12 +0300

    программа выдаёт какое число max и min

---
+++ les4/Program.cs
@@ -0,0 +1,14 @@
+﻿// какое большее какое меньшее
+Console.WriteLine("Введите число А");
+int num_A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число В");
+int num_B = Convert.ToInt32(Console.ReadLine());
+
+if (num_A > num_B)
+{
+Console.Write(" большее число = " + num_A);
+}
+{
+Console.Write(" меньшее  число = " + num_B);
+}
