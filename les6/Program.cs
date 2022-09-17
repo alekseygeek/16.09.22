176a0dfa513a7f09bd26c91892c861e06df149b5
Abashin Aleksey <mmilasomm@gmail.com>
2022-09-17 22:24:55 +0300

    написали задачу 4

--- les6/Program.cs
+++ les6/Program.cs
@@ -1,2 +1,23 @@
-﻿// начало задача 4
+﻿//  Напишите программу, которая принимает 
+//  на вход три числа и выдаёт максимальное 
+//  из этих чисел.
+ Console.Clear();
+Console.WriteLine("введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите третье число");
+int number3 = Convert.ToInt32(Console.ReadLine());
 
+int max = number1;
+
+if (number2 > max)
+{
+    max = number2;
+}
+
+if (number3 > max)
+{
+    max = number3;
+}
+Console.WriteLine("вуаля , ваше максимальное число " + max );
