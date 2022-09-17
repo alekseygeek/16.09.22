f3ddeb8d6cbcde8f11d45ac23b412a3ed5ec3b0c
Abashin Aleksey <mmilasomm@gmail.com>
2022-09-17 19:31:25 +0300

    рандомные числа добавили вывод

--- les1/Program.cs
+++ les1/Program.cs
@@ -1,5 +1,7 @@
 ﻿// рандомные числа
 int numberA = new Random().Next(1, 20);
+Console.WriteLine(numberA);
 int numberB = new Random().Next(1, 20);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
